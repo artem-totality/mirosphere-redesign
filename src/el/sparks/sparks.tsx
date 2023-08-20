e2354c000da9bdf791e145acf61ad6f1eac94a62
totality <artem.totality@gmail.com>
2023-08-20 10:38:17 +0300

    Imrove folder structure

---
+++ src/el/sparks/sparks.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './sparks.module.scss';
+
+export const Sparks = () => {
+	return (
+		<>
+			<div className={styles.firefly}></div>
+			<div className={styles.firefly}></div>
+			<div className={styles.firefly}></div>
+			<div className={styles.firefly}></div>
+			<div className={styles.firefly}></div>
+			<div className={styles.firefly}></div>
+			<div className={styles.firefly}></div>
+			<div className={styles.firefly}></div>
+			<div className={styles.firefly}></div>
+			<div className={styles.firefly}></div>
+			<div className={styles.firefly}></div>
+			<div className={styles.firefly}></div>
+			<div className={styles.firefly}></div>
+			<div className={styles.firefly}></div>
+			<div className={styles.firefly}></div>
+		</>
+	);
+};
