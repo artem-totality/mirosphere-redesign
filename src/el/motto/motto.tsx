1cfe4f07dbeb744033570d8fc6fad5913b45195f
totality <artem.totality@gmail.com>
2023-08-21 16:28:29 +0300

    Add Contacts component complete

--- src/el/motto/motto.tsx
+++ src/el/motto/motto.tsx
@@ -1,9 +1,35 @@
+'use client';
+
 import React from 'react';
 import { Container } from '@/el/container';
 import { Card } from '@/el/card';
+// @ts-ignore
+import Typed from 'typed.js';
 import styles from './motto.module.scss';
 
 export const Motto = () => {
+	// Create reference to store the DOM element containing the animation
+	const el = React.useRef(null);
+
+	React.useEffect(() => {
+		const typed = new Typed(el.current, {
+			strings: [
+				'Присоединяйся, потому что только вместе можно добиться большего!',
+				'Подключайся, потому что мы объединяем людей нацеленных на изменение мира!',
+				'Присоединяйся, потому что мы уважаем каждого члена команды, как самих себя!',
+			],
+			typeSpeed: 90,
+			backSpeed: 25,
+			loop: true,
+			startDelay: 1000,
+		});
+
+		return () => {
+			// Destroy Typed instance during cleanup to stop animation
+			typed.destroy();
+		};
+	}, []);
+
 	return (
 		<section className={styles.motto}>
 			<Container>
@@ -11,21 +37,7 @@ export const Motto = () => {
 					<div className={styles.motto__title}>Партнерам:</div>
 					<Card>
 						<div className={styles['typing-area']}>
-							<span id="typing-area"></span>
-							<div className={styles['typed-text']} id="typed-text">
-								<p>
-									Присоединяйся, потому что только вместе можно добиться
-									большего!
-								</p>
-								{/* <p>
-									Подключайся, потому что мы объединяем людей нацеленных на
-									изменение мира
-								</p>
-								<p>
-									Присоединяйся, потому что мы уважаем каждого члена команды,
-									как самих себя
-								</p> */}
-							</div>
+							<span ref={el} />
 						</div>
 					</Card>
 				</div>
