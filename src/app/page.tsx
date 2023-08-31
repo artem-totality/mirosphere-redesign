edcab7ede10255ddd83cbc34a8248067d0bee371
totality <artem.totality@gmail.com>
2023-09-01 01:00:16 +0300

    Add section promo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Main } from '@/components/main';
+import { Promo } from '@/components/promo';
 import { Development } from '@/components/development';
 
 export default function Home() {
 	return (
 		<>
 			<Main />
+			<Promo />
 			<Development />
 		</>
 	);
