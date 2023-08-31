ddf72eb0e35b3206e55d8b4ac7b00143da2fe6ad
totality <artem.totality@gmail.com>
2023-08-31 12:01:02 +0300

    Add $gap variable

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -6,64 +6,26 @@ import styles from './main.module.scss';
 
 export const Main = () => {
 	return (
-		<section className={styles.contacts}>
+		<main className="main">
 			<Container>
-				<div className={styles.contacts__inner}>
-					<SectionTitle>Контакты</SectionTitle>
-					<div className={styles.contacts__info}>
-						Разработка web&#8209;приложений любой сложности под ключ
-					</div>
-					<div className={styles.contacts__main}>
-						<div className={styles.contacts__links}>
-							<Card>
-								<div className={styles['contacts__links-inner']}>
-									<a href="tel:+79604777288" className={styles.link}>
-										<svg>
-											<use href="#phoneSvg" />
-										</svg>
-										<span className={styles.link__title}>
-											+7(960)-4-777-2-88
-										</span>
-									</a>
-									<a
-										href="https://t.me/pavel_i_tel"
-										className={styles.link}
-										target="_blank"
-									>
-										<svg>
-											<use href="#telegramSvg" />
-										</svg>
-										<span className={styles.link__title}>Telegram</span>
-									</a>
-									<a href="mailto:admin@mirosphere.org" className={styles.link}>
-										<svg>
-											<use href="#mailSvg" />
-										</svg>
-										<span className={styles.link__title}>
-											admin@mirosphere.org
-										</span>
-									</a>
-								</div>
-							</Card>
+				<div className="main__inner">
+					<div className="main__content">
+						<div className="main__title">
+							Время течет
+							<br />
+							стремительно...
 						</div>
-						<form className={styles.contacts__form}>
-							<Card>
-								<div className={styles['contacts__form-inner']}>
-									<div className={styles['contacts__form-text']}>
-										Хочу получать обновления о проекте
-									</div>
-									<input
-										type="text"
-										placeholder="ваша электронная почта"
-										required
-									/>
-									<button type="submit">Подписаться</button>
-								</div>
-							</Card>
-						</form>
+						<div className="main__divider"></div>
+						<div className="main__subtitle">
+							Миросфера <span>- порядок жизни</span>
+						</div>
+						<a href="#" className="main__link">
+							С нами
+						</a>
 					</div>
+					<div className="clock"></div>
 				</div>
 			</Container>
-		</section>
+		</main>
 	);
 };
