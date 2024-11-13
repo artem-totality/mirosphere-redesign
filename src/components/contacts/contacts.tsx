6e5113b58d84d51cc6bc6046b6717a8e7923bc20
artem-totality <artem.totality@gmail.com>
2024-11-13 14:10:34 +0000

    appearance

--- src/components/contacts/contacts.tsx
+++ src/components/contacts/contacts.tsx
@@ -10,26 +10,18 @@ export const Contacts = () => {
 			<Container>
 				<div className={styles.contacts__inner}>
 					<SectionTitle>Контакты:</SectionTitle>
-					<div className={styles.contacts__info}>
-						Разработка web&#8209;приложений любой сложности под ключ
-					</div>
+					<div className={styles.contacts__info}>Разработка web&#8209;приложений любой сложности под ключ</div>
 					<div className={styles.contacts__main}>
 						<div className={styles.contacts__links}>
 							<Card>
 								<div className={styles['contacts__links-inner']}>
-									<a href="tel:+79604777288" className={styles.link}>
+									<a href="tel:+380507777777" className={styles.link}>
 										<svg>
 											<use href="#phoneSvg" />
 										</svg>
-										<span className={styles.link__title}>
-											+7(960)-4-777-2-88
-										</span>
+										<span className={styles.link__title}>+38(050)777-77-77</span>
 									</a>
-									<a
-										href="https://t.me/pavel_i_tel"
-										className={styles.link}
-										target="_blank"
-									>
+									<a href="https://t.me/pavel_i_tel" className={styles.link} target="_blank">
 										<svg>
 											<use href="#telegramSvg" />
 										</svg>
@@ -39,9 +31,7 @@ export const Contacts = () => {
 										<svg>
 											<use href="#mailSvg" />
 										</svg>
-										<span className={styles.link__title}>
-											admin@mirosphere.org
-										</span>
+										<span className={styles.link__title}>admin@mirosphere.org</span>
 									</a>
 								</div>
 							</Card>
@@ -49,14 +39,8 @@ export const Contacts = () => {
 						<form className={styles.contacts__form}>
 							<Card>
 								<div className={styles['contacts__form-inner']}>
-									<div className={styles['contacts__form-text']}>
-										Хочу получать обновления о проекте
-									</div>
-									<input
-										type="text"
-										placeholder="ваша электронная почта"
-										required
-									/>
+									<div className={styles['contacts__form-text']}>Хочу получать обновления о проекте</div>
+									<input type="text" placeholder="ваша электронная почта" required />
 									<button type="submit">Подписаться</button>
 								</div>
 							</Card>
